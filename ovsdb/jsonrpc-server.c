d07da3fab36e77c9aefa3c62af56ff52d07839a6
Andy Zhou <azhou@nicira.com>
2015-03-16 03:03:20 -0700

    ovsdb-monitor: allow multiple jsonrpc monitors to share a single ovsdb monitor
    Store ovsdb monitor in global hmap. A newly created ovsdb monitor
    object will first search the global hmap for a possible match. If
    one is found, the existing ovsdb monitor is used instead.
    
    With this patch, jsonrpc monitor and ovsdb monitor now have N:1 mapping.
    
    Signed-off-by: Andy Zhou <azhou@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>
    
    ---
    v1->v2:
          use HMAP_INITIALIZER to initialize ovsdb_monitors
          style fixes
    
    v2->v3: no change

--- ovsdb/jsonrpc-server.c
+++ ovsdb/jsonrpc-server.c
@@ -1157,6 +1157,7 @@ ovsdb_jsonrpc_monitor_create(struct ovsdb_jsonrpc_session *s, struct ovsdb *db,
                              const struct json *request_id)
 {
     struct ovsdb_jsonrpc_monitor *m = NULL;
+    struct ovsdb_monitor *dbmon = NULL;
     struct json *monitor_id, *monitor_requests;
     struct ovsdb_error *error = NULL;
     struct shash_node *node;
@@ -1234,7 +1235,13 @@ ovsdb_jsonrpc_monitor_create(struct ovsdb_jsonrpc_session *s, struct ovsdb *db,
         }
     }
 
-    ovsdb_monitor_add_jsonrpc_monitor(m->dbmon, m);
+    dbmon = ovsdb_monitor_add(m->dbmon);
+    if (dbmon != m->dbmon) {
+        /* Found an exisiting dbmon, reuse the current one. */
+        ovsdb_monitor_remove_jsonrpc_monitor(m->dbmon, m);
+        ovsdb_monitor_add_jsonrpc_monitor(dbmon, m);
+        m->dbmon = dbmon;
+    }
 
     ovsdb_monitor_get_initial(m->dbmon);
     json = ovsdb_jsonrpc_monitor_compose_update(m, true);
