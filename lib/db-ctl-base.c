efcc760f7d29c6abffe6dbca2d30db30694b5233
Andy Zhou <azhou@nicira.com>
2015-08-04 17:44:29 -0700

    db-ctl-base: allow cmd_show_table to be used with new IDL class
    When checking for IDL class table equivalence, test for the same
    table name, rather than comparing addresses.
    
    Signed-off-by: Andy Zhou <azhou@nicira.com>

--- lib/db-ctl-base.c
+++ lib/db-ctl-base.c
@@ -1629,7 +1629,7 @@ cmd_show_find_table_by_row(const struct ovsdb_idl_row *row)
     const struct cmd_show_table *show;
 
     for (show = cmd_show_tables; show->table; show++) {
-        if (show->table == row->table->class) {
+        if (!strcmp(show->table->name, row->table->class->name)) {
             return show;
         }
     }
