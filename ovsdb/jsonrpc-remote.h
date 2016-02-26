f49295993f20bc397ff4d372c362f79a3c933d1d
Andy Zhou <azhou@ovn.org>
2016-02-25 22:54:19 -0800

    ovsdb: Make 'remote' opaque in ovsdb_jsonrpc_session
    It turns out there is no need for ovsdb_jsonrpc_session to have access
    to the remote data structure. Make it opaque by introduce the definition
    of ovsdb_jsonrpc_session later in the file, only above the functions
    that need to operate on this data structure.
    
    This change will later multi-threading locking easier because
    access to 'remote' are only done from the main process, Each thread
    only use the pointer value for matching 'remote'. There is no need
    for lock protecting 'remote'.
    
    Signed-off-by: Andy Zhou <azhou@ovn.org>

---
+++ ovsdb/jsonrpc-remote.h
@@ -0,0 +1,49 @@
+/* Copyright (c) 2016 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef OVSDB_JSONRPC_REMOTE_H
+#define OVSDB_JSONRPC_REMOTE_H 1
+
+#include <stdbool.h>
+#include "jsonrpc-server.h"
+
+struct ovsdb_jsonrpc_remote;
+struct ovsdb_jsonrpc_options;
+struct pstream;
+
+struct ovsdb_jsonrpc_remote *
+ovsdb_jsonrpc_remote_create(struct ovsdb_jsonrpc_server *svr,
+                            const char *name,
+                            const struct ovsdb_jsonrpc_options *options,
+                            struct pstream **listener);
+void ovsdb_jsonrpc_remote_destroy(struct ovsdb_jsonrpc_remote *remote);
+
+
+struct ovsdb_jsonrpc_server * ovsdb_jsonrpc_remote_get_server(
+    struct ovsdb_jsonrpc_remote *remote);
+
+bool ovsdb_jsonrpc_remote_get_status(
+    struct ovsdb_jsonrpc_remote *remote,
+    struct ovsdb_jsonrpc_remote_status *status);
+
+void ovsdb_jsonrpc_remote_run(struct ovsdb_jsonrpc_remote *remote);
+void ovsdb_jsonrpc_remote_wait(struct ovsdb_jsonrpc_remote *remote);
+
+bool ovsdb_jsonrpc_remote_options_can_change(
+    const struct ovsdb_jsonrpc_remote *remote,
+    const struct ovsdb_jsonrpc_options *new_options);
+
+#endif /* ovsdb/jsonrpc-remote.h */
