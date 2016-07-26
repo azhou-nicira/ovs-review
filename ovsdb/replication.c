d82bc76fbffe8f748f74444747208721930469d3
Andy Zhou <azhou@ovn.org>
2016-07-25 19:23:02 -0700

    ovsdb: Fix memory leak in replication logic
    Release the memory of reply message of the initial "monitor" request.
    
    Reported-at: http://openvswitch.org/pipermail/dev/2016-July/076075.html
    Signed-off-by: Andy Zhou <azhou@ovn.org>
    Acked-by: William Tu <u9012063@gmail.com>

--- ovsdb/replication.c
+++ ovsdb/replication.c
@@ -365,6 +365,10 @@ get_initial_db_state(const struct db *database)
     if (msg->type == JSONRPC_REPLY) {
         process_notification(msg->result, database->db);
     }
+
+    if (msg) {
+        jsonrpc_msg_destroy(msg);
+    }
 }
 
 static void
